047b11a4ad2c5de82c1864a13135bbbc8cd09802
Crason02 <carsonexrodgers@gmail.com>
2025-09-27 18:21:06 -0400

    fin
    finish

--- Assets/LevelScript.cs
+++ Assets/LevelScript.cs
@@ -9,11 +9,19 @@ public class LevelScript : MonoBehaviour
     public bool stop = false;
     public GameObject glass;
     public GameObject turtle;
+    public GameObject gas;
+    public GameObject water;
+    public GameObject beer;
+    public GameObject med;
 
     void Start()
     {
-        Invoke("glassFunc", 5f);
-        Invoke("turtFunc", Random.Range(2f, 10f));
+        Invoke("gasFunc", Random.Range(1f, 20f));
+        Invoke("glassFunc", Random.Range(1f, 20f));
+        Invoke("waterFunc", 15f);
+        Invoke("turtFunc", Random.Range(1f, 20f));
+        Invoke("beerFunc", Random.Range(1f, 20f));
+        Invoke("medFunc", Random.Range(1f, 20f));
     }
     void Update()
     {
@@ -39,14 +47,44 @@ public class LevelScript : MonoBehaviour
     {
         if (!stop) {
             Instantiate(glass);
-            Invoke("glassFunc", Random.Range(2f, 10f));
+            Invoke("glassFunc", Random.Range(10f, 20f));
         }
     }
     public void turtFunc()
     {
         if (!stop) {
             Instantiate(turtle);
-            Invoke("turtFunc", Random.Range(2f, 10f));
+            Invoke("turtFunc", Random.Range(10f, 20f));
+        }
+    }
+    public void gasFunc()
+    {
+        if (!stop) {
+            Instantiate(gas);
+            Invoke("gasFunc", Random.Range(10f, 20f));
+        }
+    }
+
+    public void waterFunc()
+    {
+        if (!stop) {
+            Instantiate(water);
+            Invoke("waterFunc", Random.Range(10f, 20f));
+        }
+    }
+    public void beerFunc()
+    {
+        if (!stop) {
+            Instantiate(beer);
+            Invoke("beerFunc", Random.Range(10f, 20f));
+        }
+    }
+
+    public void medFunc()
+    {
+        if (!stop) {
+            Instantiate(med);
+            Invoke("medFunc", Random.Range(10f, 20f));
         }
     }
 
